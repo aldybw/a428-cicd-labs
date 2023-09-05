e0d91d250d65f1316961674d1a13e4c09ddc4d86
aldypnj <aldycky.baguswitjaksana.tik19@mhsw.pnj.ac.id>
2023-09-05 11:58:28 +0700

    deploy app to jenkins

--- src/App.js
+++ src/App.js
@@ -2,20 +2,96 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+const App = () => {
+  //  Counter is a state initialized to 0
+  const [counter, setCounter] = useState(0);
+
+  // Function is called everytime increment button is clicked
+  const handleClick1 = () => {
+    // Counter state is incremented
+    setCounter(counter + 1);
+  };
+
+  // Function is called everytime decrement button is clicked
+  const handleClick2 = () => {
+    // Counter state is decremented
+    setCounter(counter - 1);
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "300%",
+        position: "absolute",
+        width: "100%",
+        height: "100%",
+        top: "-15%",
+      }}
+    >
+      Counter App
+      <div
+        style={{
+          fontSize: "120%",
+          position: "relative",
+          top: "10vh",
+        }}
+      >
+        {counter}
       </div>
-    );
-  }
-}
+      <div className="buttons">
+        <button
+          style={{
+            fontSize: "60%",
+            position: "relative",
+            top: "20vh",
+            marginRight: "5px",
+            backgroundColor: "green",
+            borderRadius: "8%",
+            color: "white",
+          }}
+          onClick={handleClick1}
+        >
+          Increment
+        </button>
+        <button
+          style={{
+            fontSize: "60%",
+            position: "relative",
+            top: "20vh",
+            marginLeft: "5px",
+            backgroundColor: "red",
+            borderRadius: "8%",
+            color: "white",
+          }}
+          onClick={handleClick2}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default App;
